e0ffec39e0182ea1253cdac4f87cdf971659a301
ivoeliseu <149004506+ivoeliseu@users.noreply.github.com>
2023-11-05 23:57:41 -0300

    Animation Module
    Made modifications in the code and add animation stuff.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,21 +8,39 @@ using System;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D rb;
-    [Header("Movimente Setup")]
+    [Header("Moviment Setup")]
     public float moveVelocity = 15f;
     public float runningVelocity = 30f;
     public float jumpForce = 30f;
 
+    /* *** ANTIGA CODIFICAÇÃO DE ANIMAÇÃO ***
+    
     [Header("Animation Setup")]
-    public string lookingTag = "Ground";
+    
     public float jumpScaleY = 3f;
     public float jumpScaleX = .75f;
     public float landedScaleY = .75f;
     public float landedScaleX = .3f;
     public float animationDuration = .3f;
     public Ease ease = Ease.OutBack;
-   
+
+    */
+
+    [Header("Animation Player")]
+    public string boolRun = "Run";
+    public string boolJump = "Jump";
+    public Animator animator;
+    public float swipeSideDuration = .1f;
     public bool landed = false;
+    public string lookingTag = "Ground";
+
+
+    [Header("Controls Input")]
+    public KeyCode jumpInput = KeyCode.Space;
+    public KeyCode moveLeftInput = KeyCode.LeftArrow;
+    public KeyCode moveRightInput = KeyCode.RightArrow;
+    public KeyCode runningInput = KeyCode.LeftShift;
+    
 
 
     public Vector2 friction = new Vector2(.1f, 0);
@@ -36,13 +54,38 @@ public class Player : MonoBehaviour
     {
         // Inputs de movimentação
 
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(moveLeftInput))
         {
+            animator.SetBool(boolRun, true);
+            if (rb.transform.localScale.x != -1)
+            {
+                rb.transform.DOScaleX(-1, swipeSideDuration);
             rb.velocity = new Vector2((Input.GetKey(KeyCode.LeftShift)) ? -runningVelocity : -moveVelocity, rb.velocity.y);
+            }
         }
-        else if (Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(moveRightInput))
         {
+            animator.SetBool(boolRun, true);
+            if (rb.transform.localScale.x != 1)
+            {
+                rb.transform.DOScaleX(1, swipeSideDuration);
+            }
+            
             rb.velocity = new Vector2((Input.GetKey(KeyCode.LeftShift)) ? +runningVelocity : moveVelocity, rb.velocity.y);
+        }
+        else
+        {
+            animator.SetBool(boolRun, false);
+        }
+
+        // Se verificar que Input de correr está pressionado, muda a velocidade da animação
+        if (Input.GetKey(runningInput)) 
+        {
+            animator.speed = 3f;
+        }
+        else
+        {
+            animator.speed = 1f;
         }
 
         // Desaceleração
@@ -60,17 +103,25 @@ public class Player : MonoBehaviour
     // Função que faz o jogador pular
     private void PlayerJump()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(jumpInput))
         {
-            rb.velocity = Vector2.up * jumpForce;
-            rb.transform.localScale = Vector2.one;
-            DOTween.Kill(rb.transform);
-            landed = false;
-            JumpingAnimation();
+            if (landed)
+            {
+                landed = false;
+                animator.SetBool(boolJump, true);
+                rb.velocity = Vector2.up * jumpForce;
+                rb.transform.localScale = Vector2.one;
+            }
+
+            // DOTween.Kill(rb.transform);  *** ANTIGA CODIFICAÇÃO DE ANIMAÇÃO ***
+            // landed = false; *** ANTIGA CODIFICAÇÃO DE ANIMAÇÃO ***
+            // JumpingAnimation(); *** ANTIGA CODIFICAÇÃO DE ANIMAÇÃO ***
         }
 
     }
 
+    /*  *** ANTIGA CODIFICAÇÃO DE ANIMAÇÃO ***
+    
     // Animação chamada ao pular
     public void JumpingAnimation()
     {
@@ -78,6 +129,7 @@ public class Player : MonoBehaviour
         rb.transform.DOScaleY(jumpScaleY, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
         rb.transform.DOScaleX(jumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
     }
+    
 
     // Animação ao cair na plataforma
     public void OnLandingAnimation()
@@ -85,15 +137,19 @@ public class Player : MonoBehaviour
         rb.transform.DOScaleY(landedScaleY, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
         rb.transform.DOScaleX(landedScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
     }
+    */
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (landed) return;
         if (collision.transform.tag == lookingTag)
         {
-            Debug.Log("Player colidiu com o terreno");
-            DOTween.Kill(rb.transform);
-            OnLandingAnimation();
+            // Debug.Log("Player colidiu com o terreno");
+            // DOTween.Kill(rb.transform);
+            // OnLandingAnimation();
+            animator.SetBool(boolJump, false);
             landed = true;
         }
     }
+    
 }
